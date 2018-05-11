03a0cfb51a131d4dd76c12e9da4d5f70e3f184b8
Kristen Bateson <k.bateson@comcast.net>
2018-05-10 22:49:46 -0700

    2 squares are better than one

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,57 @@ use opengl_graphics::{ GlGraphics, OpenGL };
 
 pub struct App {
     gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,   // Rotation for the square.
-    position: [f64; 2]
+    player1: ColoredRect,
+    player2: ColoredRect
+}
+
+struct ColoredRect {
+    pub color: [f32; 4],
+    pub position: [f64; 4],
+    velocity: [f64; 2]
+}
+
+impl ColoredRect {
+    fn new(color: [f32; 4], position: [f64; 4]) -> Self {
+        ColoredRect {
+            color: color,
+            position: position,
+            velocity: [0.3, 0.3]
+        }
+    }
+
+    fn update_color(color: f32)->f32 {
+        if color <= 0.0 {
+            1.0
+        } else {
+            color - 0.001
+        }
+    }
+
+    fn update(&mut self, size: (f64, f64)) {
+        self.color[0] = Self::update_color(self.color[0]);
+        self.color[1] = Self::update_color(self.color[1]);
+        self.color[2] = Self::update_color(self.color[2]);
+
+        // update X
+        if self.position[0] + self.position[2] >= size.0||
+           self.position[0] < 0.0 {
+            self.velocity[0] = -self.velocity[0];
+        }
+        self.position[0] += self.velocity[0];
+
+        // update Y
+        if self.position[1] + self.position[3] >= size.1||
+           self.position[1] < 0.0 {
+            self.velocity[1] = -self.velocity[1];
+        }
+        self.position[1] += self.velocity[1];
+    }
+
+    fn clone(&mut self) -> ColoredRect {
+        return ColoredRect::new(self.color, self.position);
+    }
+    
 }
 
 impl App {
@@ -20,45 +69,48 @@ impl App {
         use graphics::*;
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let x = self.position[0];
-        let y = self.position[1];
+        let square1: ColoredRect = self.player1.clone();
+        let square2: ColoredRect = self.player2.clone();
 
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
             // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
+            rectangle(square1.color, square1.position, c.transform, gl);
+            rectangle(square2.color, square2.position, c.transform, gl);
         });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
     }
 
     fn move_square(&mut self, args: &ButtonArgs) {
         println!("Scancode {:?}", args.scancode);
         match args.scancode {
             Some(124) => {
-                self.position[0] += 2.0;
+                self.player2.position[0] += 2.0;
             }
             Some(123) => {
-                self.position[0] -= 2.0;
+                self.player2.position[0] -= 2.0;
             }
             Some(125) => {
-                self.position[1] += 2.0;
+                self.player2.position[1] += 2.0;
             }
             Some(126) => {
-                self.position[1] -= 2.0;
+                self.player2.position[1] -= 2.0;
+            }
+            Some(2) => {
+                self.player1.position[0] += 2.0;
+            }
+            Some(0) => {
+                self.player1.position[0] -= 2.0;
+            }
+            Some(1) => {
+                self.player1.position[1] += 2.0;
+            }
+            Some(13) => {
+                self.player1.position[1] -= 2.0;
             }
             _ => {}
         }
@@ -82,8 +134,8 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-        position: [0.0, 0.0]
+        player1: ColoredRect::new([1.0, 0.0, 0.0, 1.0], [0.0, 0.0, 50.0, 50.0]),
+        player2: ColoredRect::new([0.0, 0.0, 1.0, 1.0], [50.0, 50.0, 50.0, 50.0])
     };
 
     let mut events = Events::new(EventSettings::new());
