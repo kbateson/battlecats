278dd63dec9ffc4886dc612bfd5c07728037160c
Kristen Bateson <k.bateson@comcast.net>
2018-05-10 21:56:00 -0700

    WE HAVE MOVEMENT

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,103 @@
-extern crate piston_window;
-extern crate input;
-use piston_window::*;
-use input::GenericEvent;
+extern crate piston;
+extern crate graphics;
+extern crate glutin_window;
+extern crate opengl_graphics;
 
-struct ColoredRect {
-    pub color: [f32; 4],
-    pub position: [f64; 4],
-    velocity: [f64; 2]
+use piston::window::WindowSettings;
+use piston::event_loop::*;
+use piston::input::*;
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{ GlGraphics, OpenGL };
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64,   // Rotation for the square.
+    position: [f64; 2]
 }
 
-impl ColoredRect {
-    fn new() -> Self {
-        ColoredRect {
-            color: [1.0, 0.5, 0.25, 1.0,],
-            position: [0.0, 0.0, 100.0, 100.0],
-            velocity: [0.3, 0.3]
-        }
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let x = self.position[0];
+        let y = self.position[1];
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
+
+            let transform = c.transform.trans(x, y)
+                                       .rot_rad(rotation)
+                                       .trans(-25.0, -25.0);
+
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
+        });
     }
 
-    fn update_color(color: f32)->f32 {
-        if color <= 0.0 {
-            1.0
-        } else {
-            color - 0.001
-        }
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
     }
 
-    fn update(&mut self, size: (f64, f64)) {
-        self.color[0] = Self::update_color(self.color[0]);
-        self.color[1] = Self::update_color(self.color[1]);
-        self.color[2] = Self::update_color(self.color[2]);
-
-        // update X
-        if self.position[0] + self.position[2] >= size.0||
-           self.position[0] < 0.0 {
-            self.velocity[0] = -self.velocity[0];
+    fn move_square(&mut self, args: &ButtonArgs) {
+        println!("Scancode {:?}", args.scancode);
+        match args.scancode {
+            Some(124) => {
+                self.position[0] += 2.0;
+            }
+            Some(123) => {
+                self.position[0] -= 2.0;
+            }
+            Some(125) => {
+                self.position[1] += 2.0;
+            }
+            Some(126) => {
+                self.position[1] -= 2.0;
+            }
+            _ => {}
         }
-        self.position[0] += self.velocity[0];
-
-        // update Y
-        if self.position[1] + self.position[3] >= size.1||
-           self.position[1] < 0.0 {
-            self.velocity[1] = -self.velocity[1];
-        }
-        self.position[1] += self.velocity[1];
     }
 }
 
 fn main() {
-    let mut rect = ColoredRect::new();
-    let mut window: PistonWindow =
-        WindowSettings::new("MORTAL TOMCAT", [640, 480])
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+            "spinning-square",
+            [200, 200]
+        )
+        .opengl(opengl)
         .exit_on_esc(true)
-        .vsync(true)
-        .build().unwrap();
-    
-    while let Some(e) = window.next() {
-        match e {
-            input::Render(_) => {
-                window.draw_2d(&e, |c, g| {
-                    clear([1.0; 4], g);
-                    rectangle(rect.color, 
-                                rect.position, 
-                                c.transform, g);
-                });
-            }
-            Input::Update(_) => {
-                rect.update((640.0, 480.0));
-            }
-            _ => {}
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+        position: [0.0, 0.0]
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
+
+        if let Some(b) = e.button_args() {
+            app.move_square(&b);
         }
     }
 }
