2523ae56096d375c3f56260a207f788def69ade5
Kristen Bateson <k.bateson@comcast.net>
2018-05-15 18:40:53 -0700

    adding press vs release

--- src/main.rs
+++ src/main.rs
@@ -19,19 +19,21 @@ struct Cat {
     pub color: [f32; 4],
     pub position: [f64; 4],
     pub stats: [f64; 5], // attack, spd, def, current health, total health
+    pub movement: [bool; 4] // up, down, left, right
 }
 
 impl Cat {
-    fn new(color: [f32; 4], position: [f64; 4]) -> Self {
+    fn new(color: [f32; 4], position: [f64; 4], stats: [f64; 5]) -> Self {
         Cat {
             color: color,
             position: position,
-            stats: [1.0, 1.0, 1.0, 10.0, 10.0]
+            stats: stats,
+            movement: [false, false, false, false]
         }
     }
 
     fn clone(&mut self) -> Cat {
-        return Cat::new(self.color, self.position);
+        return Cat::new(self.color, self.position, self.stats);
     }
     
 }
@@ -53,34 +55,67 @@ impl App {
         });
     }
 
-    fn move_square(&mut self, args: &ButtonArgs) {
+    fn input(&mut self, args: &ButtonArgs) {
         match args.button {
             Button::Keyboard(Key::Right) => {
-                self.player2.position[0] += 2.0;
+                self.player2.movement[3] = true;
             }
             Button::Keyboard(Key::Left) => {
-                self.player2.position[0] -= 2.0;
+                self.player2.movement[2] = true;
             }
             Button::Keyboard(Key::Down) => {
-                self.player2.position[1] += 2.0;
+                self.player2.movement[1] = true;
             }
             Button::Keyboard(Key::Up) => {
-                self.player2.position[1] -= 2.0;
+                self.player2.movement[0] = true;
             }
             Button::Keyboard(Key::D) => {
-                self.player1.position[0] += 2.0;
+                self.player1.movement[3] = true;
             }
             Button::Keyboard(Key::A) => {
-                self.player1.position[0] -= 2.0;
+                self.player1.movement[2] = true;
             }
             Button::Keyboard(Key::S) => {
-                self.player1.position[1] += 2.0;
+                self.player1.movement[1] = true;
             }
             Button::Keyboard(Key::W) => {
-                self.player1.position[1] -= 2.0;
+                self.player1.movement[0] = true;
             }
             _ => {}
         }
+    }
+
+    fn release(&mut self, args: &ButtonArgs) {
+        match args.button {
+            Button::Keyboard(Key::Right) => {
+                self.player2.movement[3] = false;
+            }
+            Button::Keyboard(Key::Left) => {
+                self.player2.movement[2] = false;
+            }
+            Button::Keyboard(Key::Down) => {
+                self.player2.movement[1] = false;
+            }
+            Button::Keyboard(Key::Up) => {
+                self.player2.movement[0] = false;
+            }
+            Button::Keyboard(Key::D) => {
+                self.player1.movement[3] = false;
+            }
+            Button::Keyboard(Key::A) => {
+                self.player1.movement[2] = false;
+            }
+            Button::Keyboard(Key::S) => {
+                self.player1.movement[1] = false;
+            }
+            Button::Keyboard(Key::W) => {
+                self.player1.movement[0] = false;
+            }
+            _ => {}
+        }
+    }
+
+    fn update(&mut self, args: &UpdateArgs) {
     }
 }
 
@@ -101,8 +136,8 @@ fn main() {
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        player1: Cat::new([1.0, 0.0, 0.0, 1.0], [20.0, 75.0, 50.0, 50.0]),
-        player2: Cat::new([0.0, 0.0, 1.0, 1.0], [130.0, 75.0, 50.0, 50.0])
+        player1: Cat::new([1.0, 0.0, 0.0, 1.0], [20.0, 75.0, 50.0, 50.0], [2.0, 0.5, 1.0, 10.0, 10.0]),
+        player2: Cat::new([0.0, 0.0, 1.0, 1.0], [130.0, 75.0, 50.0, 50.0], [1.0, 2.0, 0.5, 10.0, 10.0])
     };
 
     let mut events = Events::new(EventSettings::new());
@@ -112,7 +147,18 @@ fn main() {
         }
 
         if let Some(b) = e.button_args() {
-            app.move_square(&b);
+            match b.state {
+                ButtonState::Press => {
+                    app.input(&b);
+                }
+                ButtonState::Release => {
+                    app.release(&b);
+                }
+            }
+        }
+
+        if let Some(u) = e.update_args() {
+            app.update(&u);
         }
     }
 }
